939fd19d3251ea875358756c37325066ebda435c
ABHIJIT <abhijit.das@codelogicx.com>
2019-02-05 19:27:47 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/password/view/ChangePasswordActivity.kt
+++ app/src/main/java/com/app/l_pesa/password/view/ChangePasswordActivity.kt
@@ -79,6 +79,7 @@ class ChangePasswordActivity : AppCompatActivity() {
             }
             else
             {
+                hideKeyBoard()
                 if(CommonMethod.isNetworkAvailable(this@ChangePasswordActivity))
                 {
 
