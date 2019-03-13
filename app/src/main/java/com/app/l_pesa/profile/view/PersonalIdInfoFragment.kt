140dbcf5ab18801abd3381f623f5e4837843acd3
root <amiyo.biswas@codelogicx.com>
2019-03-13 18:10:49 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/profile/view/PersonalIdInfoFragment.kt
+++ app/src/main/java/com/app/l_pesa/profile/view/PersonalIdInfoFragment.kt
@@ -1,12 +1,26 @@
 package com.app.l_pesa.profile.view
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Dialog
+import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -35,7 +49,10 @@ import com.app.l_pesa.profile.model.ModelWindowPopUp
 import com.app.l_pesa.profile.presenter.PresenterAddProof
 import com.app.l_pesa.profile.presenter.PresenterDeleteProof
 import com.google.gson.JsonObject
-import java.util.ArrayList
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PersonalIdInfoFragment : Fragment(), ICallBackClickPersonalId, ICallBackProof {
@@ -47,6 +64,12 @@ class PersonalIdInfoFragment : Fragment(), ICallBackClickPersonalId, ICallBackPr
     var personalIdType=""
     var personalIdName=""
     var personalId=0
+
+    private val PHOTO               = 1
+    private val GALLEY              = 2
+    private var captureFile: File?  = null
+    private var imageFilePath       = ""
+    private var imageSelectStatus   : Boolean = false
 
     companion object {
         fun newInstance(): Fragment {
@@ -132,9 +155,29 @@ class PersonalIdInfoFragment : Fragment(), ICallBackClickPersonalId, ICallBackPr
         }
 
         etPersonalId.isFocusable=false
-
         etPersonalId.setOnClickListener {
             showDialogIdType(sharedPrefOBJ)
+
+        }
+
+        imgProfile.setOnClickListener {
+
+            val items = arrayOf<CharSequence>("Camera", "Gallery", "Cancel") // array list
+            val dialogView = AlertDialog.Builder(activity!!)
+            dialogView.setTitle("Choose Options")
+
+            dialogView.setItems(items) { dialog, item ->
+
+                when {
+                    items[item] == "Camera" -> // open camera
+                        cameraClick() // open default camera
+                    items[item] == "Gallery" -> // open gallery
+                        galleryClick() // open default gallery
+                    items[item] == "Cancel" -> // close dialog
+                        dialog.dismiss()
+                }
+            }
+            dialogView.show()
 
         }
 
@@ -318,6 +361,172 @@ class PersonalIdInfoFragment : Fragment(), ICallBackClickPersonalId, ICallBackPr
     override fun onFailureDeleteProof(message: String) {
         swipeRefreshLayout.isRefreshing=false
         CommonMethod.customSnackBarError(llRoot,activity!!,message)
+    }
+
+    private fun cameraClick()
+    {
+        try {
+            val imageFile = createImageFile()
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            val authorities = "com.app.l_pesa.fileprovider"
+            val imageUri = FileProvider.getUriForFile(activity!!, authorities, imageFile)
+            callCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(callCameraIntent, PHOTO)
+
+        } catch (e: IOException) {
+            Toast.makeText(activity!!,"Could not create file!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName: String = "personal_id" + timeStamp + "_"
+        val storageDir: File = activity!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        if (!storageDir.exists()) storageDir.mkdirs()
+        captureFile = File.createTempFile(imageFileName, ".jpg", storageDir)
+        imageFilePath = captureFile!!.absolutePath
+        return captureFile!!
+    }
+
+    private fun galleryClick()
+    {
+        val checkSelfPermission = ContextCompat.checkSelfPermission(activity!!, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (checkSelfPermission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+        }
+        else{
+            openAlbum()
+        }
+    }
+
+    private fun openAlbum(){
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLEY)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1 ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum()
+                }
+                else {
+                    Toast.makeText(activity!!, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            PHOTO ->
+
+                if (resultCode == Activity.RESULT_OK) {
+                    imgProfile.setImageBitmap(scaleBitmap())
+                    CommonMethod.fileCompress(captureFile!!)
+                    imageSelectStatus=true
+                } else
+
+                {
+                    if(imageSelectStatus)
+                    {
+                    }
+                    else
+                    {
+                        imageSelectStatus=false
+                    }
+
+                }
+            GALLEY ->
+                if (resultCode == Activity.RESULT_OK) {
+                    handleImage(data)
+
+                }
+        }
+    }
+
+    private fun scaleBitmap(): Bitmap
+    {
+        val imageViewWidth  = 500
+        val imageViewHeight = 500
+
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(imageFilePath, bmOptions)
+        val bitmapWidth = bmOptions.outWidth
+        val bitmapHeight = bmOptions.outHeight
+        val scaleFactor = Math.min(bitmapWidth / imageViewWidth, bitmapHeight / imageViewHeight)
+
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        return BitmapFactory.decodeFile(imageFilePath, bmOptions)
+
+    }
+
+    private fun handleImage(data: Intent?) {
+        var imagePath=""
+        val uri = data!!.data
+        if (DocumentsContract.isDocumentUri(activity!!, uri)){
+            val docId = DocumentsContract.getDocumentId(uri)
+            if ("com.android.providers.media.documents" == uri!!.authority){
+                val id = docId.split(":")[1]
+                val section = MediaStore.Images.Media._ID + "=" + id
+                imagePath = imagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, section)
+            }
+            else if ("com.android.providers.downloads.documents" == uri.authority){
+                val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(docId))
+                imagePath = imagePath(contentUri, null)
+            }
+        }
+        else if ("content".equals(uri!!.scheme, ignoreCase = true)){
+            imagePath = imagePath(uri, null)
+        }
+        else if ("file".equals(uri.scheme, ignoreCase = true)){
+            imagePath = uri.path!!
+        }
+        displayImage(imagePath)
+    }
+
+    private fun imagePath(uri: Uri?, selection: String?): String {
+        var path: String? = null
+        val cursor = activity!!.contentResolver.query(uri!!, null, selection, null, null )
+        if (cursor != null){
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+            }
+            cursor.close()
+        }
+        return path!!
+    }
+
+    private fun displayImage(imagePath: String?){
+        if (imagePath != null) {
+
+            val imgSize = File(imagePath)
+            val length  = imgSize.length() / 1024
+            if(length>3000) // Max Size Under 3MB
+            {
+                imageSelectStatus=false
+                Toast.makeText(activity, "Image size maximum 3Mb", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                imageSelectStatus=true
+                val bitmap = BitmapFactory.decodeFile(imagePath)
+                imgProfile.setImageBitmap(bitmap)
+            }
+
+        }
+        else {
+            imageSelectStatus=false
+            Toast.makeText(activity!!, "Failed to get image", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
