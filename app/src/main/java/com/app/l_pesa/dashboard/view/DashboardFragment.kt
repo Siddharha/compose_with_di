7d0754ddd7bb7167c3b14bcb3fd4882ca146d043
root <amiyo.biswas@codelogicx.com>
2019-03-22 16:26:05 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/dashboard/view/DashboardFragment.kt
+++ app/src/main/java/com/app/l_pesa/dashboard/view/DashboardFragment.kt
@@ -21,7 +21,8 @@ import com.app.l_pesa.dashboard.inter.ICallBackListOnClick
 import com.app.l_pesa.loanHistory.view.LoanPaybackScheduledActivity
 
 
-class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
+class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick{
+
 
 
     companion object {
@@ -29,9 +30,10 @@ class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
             return DashboardFragment()
         }
     }
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.dashboard_layout, container,false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.dashboard_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,19 +44,16 @@ class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
         initData()
     }
 
-    private fun initUI()
-    {
-         initSeekBar()
+    private fun initUI() {
+        initSeekBar()
     }
 
-    private fun initSeekBar()
-    {
-         seekBar.invalidate()
+    private fun initSeekBar() {
+        seekBar.setOnTouchListener { _, _ -> true }
 
     }
 
-    private fun swipeRefresh()
-    {
+    private fun swipeRefresh() {
 
         swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent)
         swipeRefreshLayout.setOnRefreshListener {
@@ -63,37 +62,30 @@ class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
         }
     }
 
-    private fun initData()
-    {
-        val sharedPrefOBJ= SharedPref(activity!!)
+    private fun initData() {
+        val sharedPrefOBJ = SharedPref(activity!!)
         val dashBoard = Gson().fromJson<ResDashboard.Data>(sharedPrefOBJ.userDashBoard, ResDashboard.Data::class.java)
 
-        if(dashBoard!=null)
-        {
+        if (dashBoard != null) {
             setDashBoard(dashBoard)
         }
 
 
     }
 
-    private fun loadDashboard()
-    {
-        if(CommonMethod.isNetworkAvailable(activity!!))
-        {
+    private fun loadDashboard() {
+        if (CommonMethod.isNetworkAvailable(activity!!)) {
             swipeRefreshLayout.isRefreshing = true
-            val sharedPrefOBJ= SharedPref(activity!!)
-            val presenterDashboard= PresenterDashboard()
-            presenterDashboard.getDashboard(activity!!,sharedPrefOBJ.accessToken,this)
-        }
-        else
-        {
+            val sharedPrefOBJ = SharedPref(activity!!)
+            val presenterDashboard = PresenterDashboard()
+            presenterDashboard.getDashboard(activity!!, sharedPrefOBJ.accessToken, this)
+        } else {
             swipeRefreshLayout.isRefreshing = false
-            CommonMethod.customSnackBarError(rootLayout,activity!!,resources.getString(R.string.no_internet))
+            CommonMethod.customSnackBarError(rootLayout, activity!!, resources.getString(R.string.no_internet))
         }
     }
 
-    private fun setDashBoard(dashBoard: ResDashboard.Data)
-    {
+    private fun setDashBoard(dashBoard: ResDashboard.Data) {
 
         setData(dashBoard)
     }
@@ -105,46 +97,43 @@ class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
 
     override fun onFailureDashboard(jsonMessage: String) {
 
-       swipeRefreshLayout.isRefreshing = false
+        swipeRefreshLayout.isRefreshing = false
     }
 
-    private fun setData(dashBoard: ResDashboard.Data)
-    {
-        val middleVal           = dashBoard.maxCreditScore /2
-        left_header_txt.text    = dashBoard.fixedDepositAmount
-        right_header_txt.text   = dashBoard.savingsAmount
-        txt_start.text          = dashBoard.minCreditScore.toString()
-        txt_middle.text         = middleVal.toString()
-        txt_max.text            = dashBoard.maxCreditScore.toString()
+    private fun setData(dashBoard: ResDashboard.Data) {
 
+        left_header_txt.text = dashBoard.fixedDepositAmount
+        right_header_txt.text = dashBoard.savingsAmount
 
-        seekBar.post {
-            seekBar.max             = dashBoard.maxCreditScore
-            seekBar.progress        = dashBoard.creditScore
-            seekBar.isEnabled       = false
+        seekBar.post{
+
+            txt_start.text          = dashBoard.minCreditScore.toString()
+            txt_max.text            = dashBoard.maxCreditScore.toString()
+
+            seekBar.max = dashBoard.maxCreditScore.toFloat()
+            seekBar.setProgress(dashBoard.creditScore.toFloat())
 
         }
 
-        if(dashBoard.loans!!.size>0)
-        {
-            loan_list.layoutManager     = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
-            val adapterDashBoard        = LoanListAdapter(dashBoard.loans!!,activity,rootLayout,this)
-            loan_list.adapter           = adapterDashBoard
-            adapterDashBoard.notifyDataSetChanged()
-        }
+            if (dashBoard.loans!!.size > 0) {
+                loan_list.layoutManager = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
+                val adapterDashBoard = LoanListAdapter(dashBoard.loans!!, activity, rootLayout, this)
+                loan_list.adapter = adapterDashBoard
+                adapterDashBoard.notifyDataSetChanged()
+            }
 
-        val sharedPrefOBJ=SharedPref(activity!!)
-        val gson                          = Gson()
-        val dashBoardData                 = gson.toJson(dashBoard)
-        sharedPrefOBJ.userDashBoard       = dashBoardData
+            val sharedPrefOBJ = SharedPref(activity!!)
+            val gson = Gson()
+            val dashBoardData = gson.toJson(dashBoard)
+            sharedPrefOBJ.userDashBoard = dashBoardData
 
     }
 
     override fun onClickLoanList(type: String) {
 
-        val sharedPref=SharedPref(activity!!)
-        sharedPref.navigationTab=resources.getString(R.string.open_tab_loan)
-        sharedPref.openTabLoan=type
+        val sharedPref = SharedPref(activity!!)
+        sharedPref.navigationTab = resources.getString(R.string.open_tab_loan)
+        sharedPref.openTabLoan = type
 
         val intent = Intent(activity, DashboardActivity::class.java)
         startActivity(intent)
@@ -152,20 +141,23 @@ class DashboardFragment: Fragment(), ICallBackDashboard, ICallBackListOnClick {
 
     }
 
-    override fun onClickPay(type: String,loan_id:String) {
+    override fun onClickPay(type: String, loan_id: String) {
 
-        val sharedPref= SharedPref(activity!!)
-        sharedPref.payFullAmount="A"
-        val bundle     = Bundle()
-        bundle.putString("LOAN_TYPE",type)
-        bundle.putString("LOAN_ID",loan_id)
+        val sharedPref = SharedPref(activity!!)
+        sharedPref.payFullAmount = "A"
+        val bundle = Bundle()
+        bundle.putString("LOAN_TYPE", type)
+        bundle.putString("LOAN_ID", loan_id)
         val intent = Intent(activity, LoanPaybackScheduledActivity::class.java)
         intent.putExtras(bundle)
-        startActivity(intent,bundle)
+        startActivity(intent, bundle)
         activity?.overridePendingTransition(R.anim.right_in, R.anim.left_out)
 
     }
 
 
 }
+
+
+
 
