e99772d0f66e2c11700b0974b5ec163a83b76e7a
root <amiyo.biswas@codelogicx.com>
2019-03-29 12:19:24 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/lpk/view/TransferHistoryFragment.kt
+++ app/src/main/java/com/app/l_pesa/lpk/view/TransferHistoryFragment.kt
@@ -1,5 +1,7 @@
 package com.app.l_pesa.lpk.view
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v4.app.Fragment
@@ -15,8 +17,8 @@ import com.app.l_pesa.lpk.inter.ICallBackTransferHistory
 import com.app.l_pesa.lpk.model.ResTransferHistory
 import com.app.l_pesa.lpk.presenter.PresenterTransferHistory
 import kotlinx.android.synthetic.main.fragment_transfer_history.*
-import java.util.ArrayList
-
+import kotlinx.android.synthetic.main.layout_filter_by_date.*
+import java.util.*
 
 
 class TransferHistoryFragment : Fragment(), ICallBackTransferHistory {
@@ -45,7 +47,7 @@ class TransferHistoryFragment : Fragment(), ICallBackTransferHistory {
         initData()
         swipeRefresh()
 
-        mBottomSheetBehavior1 = BottomSheetBehavior.from<View>(bottom_sheet1)
+        mBottomSheetBehavior1 = BottomSheetBehavior.from<View>(bottom_sheet)
         mBottomSheetBehavior1!!.isHideable=true
         mBottomSheetBehavior1!!.state = BottomSheetBehavior.STATE_HIDDEN
 
@@ -84,10 +86,105 @@ class TransferHistoryFragment : Fragment(), ICallBackTransferHistory {
             mBottomSheetBehavior1!!.setState(BottomSheetBehavior.STATE_EXPANDED)
 
         }
-        else {
+        else
+        {
             mBottomSheetBehavior1!!.setState(BottomSheetBehavior.STATE_HIDDEN)
 
         }
+
+        etFromDate.setOnClickListener {
+
+            showDatePickerFrom()
+
+        }
+
+        etToDate.setOnClickListener {
+
+            showDatePickerTo()
+
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showDatePickerFrom()
+    {
+        val c       = Calendar.getInstance()
+        val year    = c.get(Calendar.YEAR)
+        val month   = c.get(Calendar.MONTH)+1
+        val day     = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(activity!!, DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+
+            if(dayOfMonth.toString().length==1)
+            {
+                if(monthOfYear.toString().length==1)
+                {
+                    etFromDate.setText("0$dayOfMonth-0$month-$year")
+                }
+                else
+                {
+                    etFromDate.setText("0$dayOfMonth-$month-$year")
+                }
+
+            }
+            else
+            {
+                if(monthOfYear.toString().length==1)
+                {
+                    etFromDate.setText("$dayOfMonth-0$month-$year")
+                }
+                else
+                {
+                    etFromDate.setText("$dayOfMonth-$monthOfYear-$year")
+                }
+
+            }
+
+        }, year, month, day)
+
+        dpd.show()
+        dpd.datePicker.maxDate = System.currentTimeMillis()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showDatePickerTo()
+    {
+        val c       = Calendar.getInstance()
+        val year    = c.get(Calendar.YEAR)
+        val month   = c.get(Calendar.MONTH)+1
+        val day     = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(activity!!, DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+
+            if(dayOfMonth.toString().length==1)
+            {
+                if(monthOfYear.toString().length==1)
+                {
+                    etToDate.setText("0$dayOfMonth-0$month-$year")
+                }
+                else
+                {
+                    etToDate.setText("0$dayOfMonth-$month-$year")
+                }
+
+            }
+            else
+            {
+                if(monthOfYear.toString().length==1)
+                {
+                    etToDate.setText("$dayOfMonth-0$month-$year")
+                }
+                else
+                {
+                    etToDate.setText("$dayOfMonth-$monthOfYear-$year")
+                }
+
+            }
+
+        }, year, month, day)
+
+        dpd.show()
+        dpd.datePicker.maxDate = System.currentTimeMillis()
     }
 
     override fun onSuccessTransferHistory(userTransferHistory: ArrayList<ResTransferHistory.UserTransferHistory>, cursors: ResTransferHistory.Cursors?) {
