60eaf4afa9a2a734fa30a52445ddc32aa3d3b451
root <amiyo.biswas@codelogicx.com>
2019-03-29 17:44:12 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/lpk/adapter/AdapterWithdrawalHistory.kt
+++ app/src/main/java/com/app/l_pesa/lpk/adapter/AdapterWithdrawalHistory.kt
@@ -2,12 +2,16 @@ package com.app.l_pesa.lpk.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.support.v7.widget.RecyclerView
+import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.l_pesa.R
 import com.app.l_pesa.common.CommonTextRegular
+import com.app.l_pesa.common.CustomButtonRegular
 import com.app.l_pesa.lpk.model.ResWithdrawalHistory
 
 class AdapterWithdrawalHistory (val context: Context, private val listWithdrawalHistory: ArrayList<ResWithdrawalHistory.UserWithdrawalHistory>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -21,7 +25,7 @@ class AdapterWithdrawalHistory (val context: Context, private val listWithdrawal
         val inflater = LayoutInflater.from(context)
         return if (viewType == 0)
         {
-            UserViewHolder(inflater.inflate(R.layout.layout_interest_history, parent, false))
+            UserViewHolder(inflater.inflate(R.layout.layout_withdrawal_history, parent, false))
         } else {
             LoadingViewHolder(inflater.inflate(R.layout.layout_load_more, parent, false))
         }
@@ -70,21 +74,60 @@ class AdapterWithdrawalHistory (val context: Context, private val listWithdrawal
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        var txtTokenValue          : CommonTextRegular = itemView.findViewById(R.id.txtTokenValue) as CommonTextRegular
-        var txtAmount              : CommonTextRegular = itemView.findViewById(R.id.txtAmount) as CommonTextRegular
-        var txtNarration           : CommonTextRegular = itemView.findViewById(R.id.txtNarration) as CommonTextRegular
-        var txtRef                 : CommonTextRegular = itemView.findViewById(R.id.txtRef) as CommonTextRegular
+        private var txtReqToken        : CommonTextRegular = itemView.findViewById(R.id.txtReqToken) as CommonTextRegular
+        private var txtStatus          : CommonTextRegular = itemView.findViewById(R.id.txtStatus) as CommonTextRegular
+        private var buttonStatus       : CustomButtonRegular = itemView.findViewById(R.id.buttonStatus) as CustomButtonRegular
 
 
         @SuppressLint("SetTextI18n", "CheckResult", "SimpleDateFormat")
         fun  bindData(context: Context, userWithdrawalHistory: ResWithdrawalHistory.UserWithdrawalHistory)
         {
-            txtTokenValue.text = context.getString(R.string.actual_token_values)+":"+userWithdrawalHistory.identity_number
+            txtReqToken.text = context.getString(R.string.request_token)+": "+userWithdrawalHistory.token_value
+
+            if(userWithdrawalHistory.status=="R" )
+            {
+                buttonStatus.visibility=View.INVISIBLE
+                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#D2322D'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+            }
+            else if(userWithdrawalHistory.status=="P" )
+            {
+                buttonStatus.visibility=View.INVISIBLE
+                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#4e6485'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+            }
+            else if(userWithdrawalHistory.status=="C" )
+            {
+                buttonStatus.text=userWithdrawalHistory.statusTxt
+                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00695c'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+            }
+            else if(userWithdrawalHistory.status=="F" )
+            {
+                buttonStatus.text=userWithdrawalHistory.statusTxt
+                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#de970e'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+            }
+            else if(userWithdrawalHistory.status=="N" )
+            {
+                buttonStatus.text=userWithdrawalHistory.statusTxt
+                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00bfa5'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+            }
+            else
+            {
+
+            }
 
 
         }
 
+        private fun fromHtml(source: String): Spanned {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                Html.fromHtml(source)
+            }
+        }
+
     }
+
+
 
     class LoadingViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
