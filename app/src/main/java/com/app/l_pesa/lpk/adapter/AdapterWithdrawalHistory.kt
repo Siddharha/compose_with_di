d55be0499bbe41562f95608b982a6ebfe18210dd
root <amiyo.biswas@codelogicx.com>
2019-04-04 15:10:33 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/lpk/adapter/AdapterWithdrawalHistory.kt
+++ app/src/main/java/com/app/l_pesa/lpk/adapter/AdapterWithdrawalHistory.kt
@@ -14,6 +14,11 @@ import com.app.l_pesa.common.CommonMethod
 import com.app.l_pesa.common.CommonTextRegular
 import com.app.l_pesa.common.CustomButtonRegular
 import com.app.l_pesa.lpk.model.ResWithdrawalHistory
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
+import java.lang.Exception
+
 
 class AdapterWithdrawalHistory (val context: Context, private val listWithdrawalHistory: ArrayList<ResWithdrawalHistory.UserWithdrawalHistory>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -88,57 +93,92 @@ class AdapterWithdrawalHistory (val context: Context, private val listWithdrawal
             txtReqToken.text = context.getString(R.string.request_token)+": "+userWithdrawalHistory.token_value
             txtCreateDate.text = CommonMethod.dateConvert(userWithdrawalHistory.created)
 
-            if(userWithdrawalHistory.status=="R" )
-            {
-                buttonStatus.visibility=View.INVISIBLE
-                txtReason.visibility=View.GONE
-                txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_up_arrow, 0)
-                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#D2322D'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
-                txtReason.text=userWithdrawalHistory.reject_reason
+            when {
+                userWithdrawalHistory.status=="R" -> {
 
-                txtStatus.setOnClickListener {
+                    buttonStatus.visibility=View.INVISIBLE
+                    txtReason.visibility=View.GONE
+                    txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_down_arrow, 0)
+                    txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#D2322D'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+                    txtReason.text=userWithdrawalHistory.reject_reason
 
-                    if(txtReason.visibility==View.VISIBLE)
-                    {
-                        txtReason.visibility=View.GONE
+                    txtStatus.setOnClickListener {
+
+                        if(txtReason.visibility==View.VISIBLE)
+                        {
+                            txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_down_arrow, 0)
+                            txtReason.visibility=View.GONE
+                        }
+                        else
+                        {
+                            txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_up_arrow, 0)
+                            txtReason.visibility=View.VISIBLE
+                        }
                     }
-                    else
-                    {
-                        txtReason.visibility=View.VISIBLE
+
+                }
+                userWithdrawalHistory.status=="P" -> {
+                    buttonStatus.visibility=View.INVISIBLE
+                    txtReason.visibility=View.GONE
+                    txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#4e6485'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+                }
+                userWithdrawalHistory.status=="C" -> {
+                    buttonStatus.visibility=View.VISIBLE
+                    txtReason.visibility=View.GONE
+                    txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00695c'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+                    buttonStatus.setOnClickListener {
+
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW)
+                            intent.data = Uri.parse(userWithdrawalHistory.real_etherscan_url)
+                            context.startActivity(intent)
+                        }
+                        catch (exp:Exception)
+                        {}
+
                     }
                 }
-            }
-            else if(userWithdrawalHistory.status=="P" )
-            {
-                buttonStatus.visibility=View.INVISIBLE
-                txtReason.visibility=View.GONE
-                txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
-                txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#4e6485'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
-            }
-            else if(userWithdrawalHistory.status=="C" )
-            {
-                 buttonStatus.visibility=View.VISIBLE
-                 txtReason.visibility=View.GONE
-                 txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
-                 txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00695c'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
-            }
-            else if(userWithdrawalHistory.status=="F" )
-            {
-                 buttonStatus.visibility=View.VISIBLE
-                 txtReason.visibility=View.GONE
-                 txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
-                 txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#de970e'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
-            }
-            else if(userWithdrawalHistory.status=="N" )
-            {
-                  buttonStatus.visibility=View.VISIBLE
-                  txtReason.visibility=View.GONE
-                  txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
-                  txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00bfa5'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
-            }
-            else
-            {
+                userWithdrawalHistory.status=="F" -> {
+                    buttonStatus.visibility=View.VISIBLE
+                    txtReason.visibility=View.GONE
+                    txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#de970e'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
 
+                    buttonStatus.setOnClickListener {
+
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW)
+                            intent.data = Uri.parse(userWithdrawalHistory.real_etherscan_url)
+                            context.startActivity(intent)
+                        }
+                        catch (exp:Exception)
+                        {}
+
+                    }
+                }
+                userWithdrawalHistory.status=="N" -> {
+                    buttonStatus.visibility=View.VISIBLE
+                    txtReason.visibility=View.GONE
+                    txtStatus.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    txtStatus.text=fromHtml(context.resources.getString(R.string.status)+"<font color='#00bfa5'>"+" "+userWithdrawalHistory.statusTxt+"</font>")
+
+                    buttonStatus.setOnClickListener {
+
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW)
+                            intent.data = Uri.parse(userWithdrawalHistory.real_etherscan_url)
+                            context.startActivity(intent)
+                        }
+                        catch (exp:Exception)
+                        {}
+
+                    }
+                }
+                else -> {
+
+                }
             }
 
 
