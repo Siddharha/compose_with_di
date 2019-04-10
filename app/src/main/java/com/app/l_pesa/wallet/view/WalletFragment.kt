9aec7670ae1aeda69c11fead03361a8336c2cd90
root <amiyo.biswas@codelogicx.com>
2019-04-10 18:42:36 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/wallet/view/WalletFragment.kt
+++ app/src/main/java/com/app/l_pesa/wallet/view/WalletFragment.kt
@@ -2,10 +2,13 @@ package com.app.l_pesa.wallet.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.l_pesa.R
+import com.app.l_pesa.common.CommonMethod
+import kotlinx.android.synthetic.main.fragment_wallet.*
 
 class WalletFragment :Fragment() {
 
@@ -23,6 +26,32 @@ class WalletFragment :Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        CommonMethod.hideKeyboardView(activity as AppCompatActivity)
+        initData()
 
+    }
+
+    private fun initData()
+    {
+
+        buttonWithdraw.setOnClickListener {
+
+            if(etWithdrawalAmount.text.toString().isEmpty())
+            {
+                CommonMethod.customSnackBarError(rootLayout,activity!!,resources.getString(R.string.required_withdrawal_amount))
+            }
+            else
+            {
+                if(CommonMethod.isNetworkAvailable(activity!!))
+                {
+
+                }
+                else
+                {
+                    CommonMethod.customSnackBarError(rootLayout,activity!!,resources.getString(R.string.no_internet))
+                }
+            }
+
+        }
     }
 }
