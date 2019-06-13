964451069c220db2ea63e851f69199ede48785e8
root <amiyo.biswas@codelogicx.com>
2019-06-13 15:01:32 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/splash/view/SplashActivity.kt
+++ app/src/main/java/com/app/l_pesa/splash/view/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.app.l_pesa.splash.view
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
@@ -19,34 +20,67 @@ import com.app.l_pesa.splash.presenter.PresenterCountry
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_splash.*
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.view.animation.ScaleAnimation
+
 
 
 class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
 
 
+    private lateinit  var animationSet: AnimationSet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        val viewWidth = view_progress.getWidth()
+        val move = TranslateAnimation((-(getScreenWidth() / 2) + viewWidth / 2).toFloat(), (getScreenWidth() / 2 + viewWidth / 2 + viewWidth).toFloat(), 0f, 0f)
+        move.setDuration(1000);
+        val move1 = TranslateAnimation((-viewWidth).toFloat(), 0f, 0f, 0f)
+        move1.setDuration(500);
+        val laftOut = ScaleAnimation(0f, 1f, 1f, 1f)
+        laftOut.duration = 500
+
+
+        animationSet = AnimationSet(true);
+        animationSet.addAnimation(move);
+        animationSet.addAnimation(move1);
+        animationSet.addAnimation(laftOut);
+        animationSet.addAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slideout));
+
+        startAnimation()
         initUI()
 
+    }
+
+    private fun startAnimation() {
+        view_progress.startAnimation(animationSet)
+        android.os.Handler().postDelayed({ startAnimation() }, 1000)
+    }
+
+    fun getScreenWidth(): Int {
+        return Resources.getSystem().getDisplayMetrics().widthPixels
     }
 
 
     private fun initUI()
     {
         val sharedPrefOBJ = SharedPref(this@SplashActivity)
-        if (sharedPrefOBJ.accessToken != resources.getString(R.string.init))
+        if (sharedPrefOBJ.accessToken != resources.getString(com.app.l_pesa.R.string.init))
         {
             if(CommonMethod.isNetworkAvailable(this@SplashActivity))
             {
-               progressBar.visibility = View.VISIBLE
+               frameProgress.visibility = View.VISIBLE
                logoutProcess()
 
             }
             else {
                 buttonRetry.visibility = View.VISIBLE
-                progressBar.visibility = View.INVISIBLE
+                frameProgress.visibility = View.INVISIBLE
 
                 buttonRetry.setOnClickListener {
 
@@ -55,7 +89,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
                         logoutProcess()
                     } else
                     {
-                        CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(R.string.no_internet))
+                        CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(com.app.l_pesa.R.string.no_internet))
                     }
 
                 }
@@ -85,7 +119,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
     {
 
         val sharedPrefOBJ = SharedPref(this@SplashActivity)
-        if (sharedPrefOBJ.countryList==resources.getString(R.string.init))
+        if (sharedPrefOBJ.countryList==resources.getString(com.app.l_pesa.R.string.init))
         {
             if(CommonMethod.isNetworkAvailable(this@SplashActivity))
             {
@@ -95,7 +129,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
                 txtTitle.visibility = View.VISIBLE
                 txtHeader.visibility = View.VISIBLE
                 buttonRetry.visibility = View.VISIBLE
-                progressBar.visibility = View.INVISIBLE
+                frameProgress.visibility = View.INVISIBLE
 
                 buttonRetry.setOnClickListener {
 
@@ -104,7 +138,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
                         loadCountry()
                     } else
                     {
-                        CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(R.string.no_internet))
+                        CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(com.app.l_pesa.R.string.no_internet))
                     }
 
                 }
@@ -113,7 +147,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
         }
         else
         {
-            progressBar.visibility = View.VISIBLE
+            frameProgress.visibility = View.VISIBLE
             splashLoading()
         }
 
@@ -123,7 +157,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
     private fun splashLoading() {
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            overridePendingTransition(R.anim.right_in, R.anim.left_out)
+            overridePendingTransition(com.app.l_pesa.R.anim.right_in, com.app.l_pesa.R.anim.left_out)
             finish()
         }, 2000)
     }
@@ -133,7 +167,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
         txtTitle.visibility = View.INVISIBLE
         txtHeader.visibility = View.INVISIBLE
         buttonRetry.visibility = View.INVISIBLE
-        progressBar.visibility = View.VISIBLE
+        frameProgress.visibility = View.VISIBLE
         val presenterCountry = PresenterCountry()
         presenterCountry.getCountry(this@SplashActivity, this)
     }
@@ -143,23 +177,23 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
         val json = Gson().toJson(countries_list)
         val sharedPrefOBJ = SharedPref(this@SplashActivity)
         sharedPrefOBJ.countryList = json
-        progressBar.visibility = View.INVISIBLE
+        frameProgress.visibility = View.INVISIBLE
 
         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-        overridePendingTransition(R.anim.right_in, R.anim.left_out)
+        overridePendingTransition(com.app.l_pesa.R.anim.right_in, com.app.l_pesa.R.anim.left_out)
         finish()
     }
 
     override fun onEmptyCountry() {
-        showSnackBar(resources.getString(R.string.no_country))
+        showSnackBar(resources.getString(com.app.l_pesa.R.string.no_country))
         buttonRetry.visibility  =View.VISIBLE
-        progressBar.visibility  =View.INVISIBLE
+        frameProgress.visibility  =View.INVISIBLE
     }
 
     override fun onFailureCountry(jsonMessage: String) {
         showSnackBar(jsonMessage)
         buttonRetry.visibility  =View.VISIBLE
-        progressBar.visibility  =View.INVISIBLE
+        frameProgress.visibility  =View.INVISIBLE
         buttonRetry.setOnClickListener {
 
             if (CommonMethod.isNetworkAvailable(this@SplashActivity))
@@ -167,7 +201,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
                 loadCountry()
             } else
             {
-                CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(R.string.no_internet))
+                CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  resources.getString(com.app.l_pesa.R.string.no_internet))
             }
 
         }
@@ -175,7 +209,7 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
     }
 
     private fun showSnackBar(message: String) {
-        progressBar.visibility = View.INVISIBLE
+        frameProgress.visibility = View.INVISIBLE
         CommonMethod.customSnackBarError(rootLayout!!,this@SplashActivity,  message)
     }
 
@@ -199,9 +233,9 @@ class SplashActivity : AppCompatActivity(), ICallBackCountry, ICallBackLogout {
     {
         val sharedPrefOBJ= SharedPref(this@SplashActivity)
         sharedPrefOBJ.removeShared()
-        progressBar.visibility = View.INVISIBLE
+        frameProgress.visibility = View.INVISIBLE
         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-        overridePendingTransition(R.anim.right_in, R.anim.left_out)
+        overridePendingTransition(com.app.l_pesa.R.anim.right_in, com.app.l_pesa.R.anim.left_out)
         finish()
     }
 
