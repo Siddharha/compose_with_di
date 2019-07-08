538da4b8de3496c05acd0854519234ce57a82100
root <amiyo.biswas@codelogicx.com>
2019-07-08 18:07:16 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/calculator/view/LoanCalculatorFragment.kt
+++ app/src/main/java/com/app/l_pesa/calculator/view/LoanCalculatorFragment.kt
@@ -1,11 +1,23 @@
 package com.app.l_pesa.calculator.view
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_loan_calculator.*
+import android.text.Spannable
+import com.app.l_pesa.common.CustomTypeFaceSpan
+import android.text.SpannableString
+import android.graphics.Typeface
+import androidx.core.content.ContextCompat
 import com.app.l_pesa.R
+
 
 class LoanCalculatorFragment:Fragment() {
 
@@ -16,12 +28,49 @@ class LoanCalculatorFragment:Fragment() {
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_loan_calculator, container,false)
+        return inflater.inflate(com.app.l_pesa.R.layout.fragment_loan_calculator, container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        initData()
     }
+
+    private fun initData()
+    {
+
+        ti_loan_type.setOnClickListener {
+
+            val popupMenuOBJ = PopupMenu(activity!!, ti_loan_type)
+            popupMenuOBJ.menuInflater.inflate(R.menu.menu_loan_type, popupMenuOBJ.menu)
+
+            popupMenuOBJ.setOnMenuItemClickListener { item: MenuItem? ->
+
+                ti_loan_type.setText(item!!.title)
+                ti_loan_type.setBackgroundColor(Color.TRANSPARENT)
+                ti_loan_type.backgroundTintList=(ContextCompat.getColorStateList(activity!!, android.R.color.transparent))
+
+                true
+            }
+
+            val menu = popupMenuOBJ.menu
+            for (i in 0 until menu.size()) {
+                val mi = menu.getItem(i)
+                applyFontToMenuItem(mi)
+            }
+
+            popupMenuOBJ.show()
+        }
+    }
+
+    private fun applyFontToMenuItem(mi: MenuItem) {
+
+        val font = Typeface.createFromAsset(activity!!.assets, "fonts/Montserrat-Regular.ttf")
+        val mNewTitle = SpannableString(mi.title)
+        mNewTitle.setSpan(CustomTypeFaceSpan("", font, Color.parseColor("#535559")), 0, mNewTitle.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        mi.title = mNewTitle
+    }
+
 }
