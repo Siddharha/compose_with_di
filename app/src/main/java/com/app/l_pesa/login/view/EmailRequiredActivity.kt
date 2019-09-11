d98691723c263b1b9a95653ea1c3b5378338e621
root <amiyo.biswas@codelogicx.com>
2019-09-11 13:23:24 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/login/view/EmailRequiredActivity.kt
+++ app/src/main/java/com/app/l_pesa/login/view/EmailRequiredActivity.kt
@@ -1,9 +1,12 @@
 package com.app.l_pesa.login.view
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.graphics.Typeface
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +14,11 @@ import com.app.l_pesa.R
 import com.app.l_pesa.common.CommonMethod
 
 import kotlinx.android.synthetic.main.activity_email_required.*
+import kotlinx.android.synthetic.main.content_email_required.*
 
 class EmailRequiredActivity : AppCompatActivity() {
+
+    private lateinit var  progressDialog   : ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,60 @@ class EmailRequiredActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbarFont(this@EmailRequiredActivity)
 
+        initLoader()
 
+        btnSubmit.setOnClickListener {
+
+            verifyField()
+        }
+
+        etEmail.setOnEditorActionListener { _, actionId, _ ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                verifyField()
+                handled = true
+            }
+            handled
+        }
+
+    }
+
+    private fun verifyField()
+    {
+        if(TextUtils.isEmpty(etEmail.text.toString()) || !CommonMethod.isValidEmailAddress(etEmail.text.toString()))
+        {
+            CommonMethod.customSnackBarError(rootLayout,this@EmailRequiredActivity,resources.getString(R.string.required_email))
+        }
+        else
+        {
+            if(CommonMethod.isNetworkAvailable(this@EmailRequiredActivity))
+            {
+
+            }
+            else
+            {
+                CommonMethod.customSnackBarError(rootLayout, this@EmailRequiredActivity, resources.getString(R.string.no_internet))
+            }
+        }
+    }
+
+    private fun initLoader()
+    {
+        progressDialog = ProgressDialog(this@EmailRequiredActivity,R.style.MyAlertDialogStyle)
+        progressDialog.isIndeterminate = true
+        progressDialog.setMessage(resources.getString(R.string.loading))
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        progressDialog.setCancelable(false)
+        progressDialog.setCanceledOnTouchOutside(false)
+
+    }
+
+    private fun dismiss()
+    {
+        if(progressDialog.isShowing)
+        {
+            progressDialog.dismiss()
+        }
     }
 
 
