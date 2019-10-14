b64a2fb6c26a9bf6ab3fecd7451e419853e9c941
root <amiyo.biswas@codelogicx.com>
2019-10-14 11:37:32 +0530

    Code rectify

--- app/src/main/java/com/app/l_pesa/API/RetrofitHelper.kt
+++ app/src/main/java/com/app/l_pesa/API/RetrofitHelper.kt
@@ -1,7 +1,7 @@
 package com.app.l_pesa.API
 
 import com.app.l_pesa.BuildConfig
-import com.app.l_pesa.BuildConfig.BASE_URL_DEV
+import com.app.l_pesa.BuildConfig.BASE_URL
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -58,7 +58,7 @@ class RetrofitHelper {
             else
             {*/
                 val retrofit = Retrofit.Builder()
-                        .baseUrl(BASE_URL_DEV)//BASE_URL_DEV//BASE_URL
+                        .baseUrl(BASE_URL)//BASE_URL_DEV//BASE_URL
                         .addConverterFactory(GsonConverterFactory.create())
                         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                         .build()
@@ -82,7 +82,7 @@ class RetrofitHelper {
             else
             {
               */  val retrofit = Retrofit.Builder()
-                        .baseUrl(BASE_URL_DEV) //BASE_URL_DEV//BASE_URL
+                        .baseUrl(BASE_URL) //BASE_URL_DEV//BASE_URL
                         .client(getOkHttpClient(accessToken))
                         .addConverterFactory(GsonConverterFactory.create())
                         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -91,7 +91,6 @@ class RetrofitHelper {
             //}
 
         }
-
 
     }
 }
