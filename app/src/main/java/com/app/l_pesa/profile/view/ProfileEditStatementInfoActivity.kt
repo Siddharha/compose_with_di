2f0f5f6bc33f9be782ea2497176b4afff5602f30
siddharthamaji <siddhartha.ee.13@gmail.com>
2020-10-16 14:30:26 +0530

    statement upload module p-2

--- app/src/main/java/com/app/l_pesa/profile/view/ProfileEditStatementInfoActivity.kt
+++ app/src/main/java/com/app/l_pesa/profile/view/ProfileEditStatementInfoActivity.kt
@@ -5,24 +5,40 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Typeface
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.*
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.app.l_pesa.BuildConfig
 import com.app.l_pesa.R
 import com.app.l_pesa.common.SharedPref
 import com.app.l_pesa.dashboard.view.DashboardActivity
+import com.app.l_pesa.loanplan.adapter.PersonalIdAdapter
+import com.app.l_pesa.profile.adapter.PersonalIdListAdapter
+import com.app.l_pesa.profile.inter.ICallBackClickPersonalId
+import com.app.l_pesa.profile.model.ResUserInfo
+import com.facebook.appevents.AppEventsConstants
+import com.facebook.appevents.AppEventsLogger
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_profile_edit_id_info.toolbar
 import kotlinx.android.synthetic.main.activity_profile_edit_statement_info.*
 import kotlinx.android.synthetic.main.add_statement_bottomsheet_layout.view.*
 import java.io.File
+import java.net.URI
+import java.util.ArrayList
 
-class ProfileEditStatementInfoActivity : AppCompatActivity() {
-
+lateinit var captureFilePath: Uri
+class ProfileEditStatementInfoActivity : AppCompatActivity(), ICallBackClickPersonalId {
+    lateinit var listPersonalIdTEst:ArrayList<ResUserInfo.UserIdsPersonalInfo>
+    private lateinit var personalIdAdapter:PersonalIdAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_edit_statement_info)
@@ -30,10 +46,32 @@ class ProfileEditStatementInfoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbarFont(this)
-
+        initData()
         onActionPerform()
     }
 
+    private fun initData() {
+
+        val logger = AppEventsLogger.newLogger(this)
+        val params = Bundle()
+        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_TYPE, "Personal Id Information")
+        logger.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, params)
+
+        listPersonalIdTEst = ArrayList()
+        val sharedPrefOBJ = SharedPref(this)
+        val profileInfo = Gson().fromJson<ResUserInfo.Data>(sharedPrefOBJ.profileInfo, ResUserInfo.Data::class.java)
+        listPersonalIdTEst.clear()
+
+        if (profileInfo.userIdsPersonalInfo!!.size > 0) {
+            cvNoItm.visibility = View.GONE
+            listPersonalIdTEst.addAll(profileInfo.userIdsPersonalInfo!!)
+            personalIdAdapter = PersonalIdAdapter(this, listPersonalIdTEst, this)
+            rlStatements.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+            rlStatements.adapter = personalIdAdapter
+        }else{
+            cvNoItm.visibility = View.VISIBLE
+        }
+    }
     private fun onActionPerform() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
@@ -95,11 +133,40 @@ class ProfileEditStatementInfoActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        if (requestCode == 165) {
+            if (resultCode == RESULT_OK) {
+                val uri = intent?.data
+               // val m_data = data
+                Log.d("Hello", captureFilePath.path!!)
+         //       val fileImagePath = getRealPathFromURI(uri!!)
+             //   val type = intent.type
 
-
+            //    Log.d("Hello", fileImagePath + "");
+//                if (uri != null) {
+//                    var path = uri.toString()
+//                    if (path.toLowerCase().startsWith("file://")) {
+//                        // Selected file/directory path is below
+//                        path = ( File(URI.create(path))).absolutePath
+//                        Log.d("Hel", path)
+//                    }
+//
+//                }
+            } else{
+                Log.d("Hello", "Back from pick with cancel status");
+        }
     }
+    }
+
+    override fun onClickIdList(userIdsPersonalInfo: ResUserInfo.UserIdsPersonalInfo, position: Int, it: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSelectIdType(id: Int, name: String, type: String) {
+        TODO("Not yet implemented")
+    }
+
+
 }
 
 class AddStatementBottomsheet(activity: Activity) : BottomSheetDialogFragment() {
@@ -116,6 +183,7 @@ class AddStatementBottomsheet(activity: Activity) : BottomSheetDialogFragment() 
     fun onActionPerform(v:View){
         v.btnBrowse.setOnClickListener {
             openPDF(activity)
+            v.tvFileName.text = "statement.pdf"
         }
         v.buttonCancel.setOnClickListener {
             dismiss()
@@ -127,7 +195,7 @@ class AddStatementBottomsheet(activity: Activity) : BottomSheetDialogFragment() 
 }
 
 private fun openPDF(activity: Activity) {
-     lateinit var captureFilePath: Uri
+
     val filePath = File(activity.filesDir, "images")
     val pdfFile = File(filePath, "statement.pdf")
     if (pdfFile.exists()) {
@@ -138,6 +206,29 @@ private fun openPDF(activity: Activity) {
     captureFilePath = FileProvider.getUriForFile(activity.baseContext, BuildConfig.APPLICATION_ID + ".provider", pdfFile)
     val intent =  Intent(Intent.ACTION_GET_CONTENT)
     intent.type = "application/pdf"
+   // intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+   // intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+   // intent.addCategory(Intent.CATEGORY_OPENABLE)
     intent.data = captureFilePath
-    activity.startActivityForResult(intent, 100)
+    activity.startActivityForResult(intent, 165)
+
+
+
+//    val intent = Intent(Intent.ACTION_GET_CONTENT);
+//    intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//        intent.setType(mimeTypes.length == 1 ? mimeTypes[0] : "*/*");
+//        if (mimeTypes.length > 0) {
+//            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+//        }
+//    } else {
+//        String mimeTypesStr = "";
+//        for (String mimeType : mimeTypes) {
+//            mimeTypesStr += mimeType + "|";
+//        }
+//        intent.setType(mimeTypesStr.substring(0,mimeTypesStr.length() - 1));
+//    }
+//    startActivityForResult(Intent.createChooser(intent,"ChooseFile"), REQUEST_CODE_DOC);
+
 }
